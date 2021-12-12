695dd5dc31b703baeee5879ab0f29fbd6f349059
Moritz Wilksch <moritzwilksch@gmail.com>
2021-12-12 14:39:04 -0500

    report

--- scripts/analysis_script_rentals.R
+++ scripts/analysis_script_rentals.R
@@ -1,92 +1,4 @@
-library(arrow)
-library(dplyr)
-library(ggplot2)
-library(lme4)
-library(lattice)
-library(xtable)
-options(xtable.comment = FALSE)
 
-OUTPUT_TABLES = TRUE
-
-dfrent = read_parquet("data/dfrent.parquet")
-dfbuy = read_parquet("data/dfbuy.parquet")
-
-#rent_cutoff = quantile(dfrent$price, 0.99)
-#buy_cutoff = quantile(dfbuy$price, 0.99)
-#dfrent = dfrent %>% filter(price <= rent_cutoff)
-#dfbuy = dfbuy %>% filter(price <= buy_cutoff)
-
-# square_meters outliers
-dfrent_cutoff = quantile(dfrent$square_meters, 0.999, na.rm=TRUE)
-dfbuy_cutoff = quantile(dfbuy$square_meters, 0.999, na.rm = TRUE)
-
-nrow(dfrent %>% filter(square_meters > dfrent_cutoff))  # removes 21 outliers
-nrow(dfbuy %>% filter(square_meters > dfbuy_cutoff))  # removes 32 outliers
-
-dfrent = dfrent %>% filter(square_meters <= dfrent_cutoff)
-dfbuy = dfbuy %>% filter(square_meters <= dfbuy_cutoff)
-
-# Rent prices
-ggplot(data=dfrent, aes(x=log(price))) + geom_histogram(bins=60) + theme_bw()  # some zeros
-
-# Buy prices
-ggplot(data=dfbuy, aes(x=price)) + geom_histogram(bins=60) + theme_bw() # many zeros
-
-####################################################
-################### RENTAL UNITS ###################
-####################################################
-
-x = dfbuy %>% filter(price > quantile(dfbuy$price, 0.999))
-
-################### EDA ###################
-
-# price by sqm
-ggplot(data=dfrent, aes(x=square_meters, y=price, color=private_offer)) + geom_point(alpha=0.1) + theme_bw() + geom_smooth(method='lm')
-
-# rooms * sqm interaction
-# same direction, but different slopes per n_rooms
-ggplot(data=dfrent, aes(x=square_meters, y=price)) + geom_point(alpha=0.5) + geom_smooth(method="lm") + facet_grid(~ rooms)
-
-
-
-
-################### Modelling - Non-hierarchical ###################
-# all mean effects
-model0 = lm(log(price) ~ object_type + private_offer + rooms + square_meters, data=dfrent)
-summary(model0)
-# plot(model0)
-
-
-plot(model0, which=4) # high cooks dist
-
-
-problematic = c(23065, 14691, 30143)
-
-# refit
-leverage_removed = dfrent[-problematic, ]
-
-write_parquet(leverage_removed, "data/intermediaries/rentals_leverage_removed.parquet")
-model0 = lm(log(price) ~ object_type + private_offer + rooms + square_meters, data=leverage_removed)
-summary(model0)
-# plot(model0)
-
-if(OUTPUT_TABLES){
-  print(xtable(summary(model0)), file="documents/scripts_output/rentals_model0_summary.tex")
-}
-
-# compdf = data.frame(ytrue=dfrent$price, yhat=exp(predict(model0, dfrent)))
-# ggplot(data=compdf, aes(x=ytrue, y=yhat)) + geom_point(alpha=0.1)
-
-# room x sqm interaction
-model1 = lm(log(price) ~ object_type + private_offer + rooms * square_meters, data=leverage_removed)
-summary(model1)
-# plot(model1)
-
-if(OUTPUT_TABLES){
-  print(xtable(summary(model1)), file="documents/scripts_output/rentals_model1_summary.tex")
-}
-
-anova(model1, model0)
 
 ################### Modelling - Hierarchical ###################
 leverage_removed$std_log_price = scale(log(leverage_removed$price))
@@ -124,8 +36,12 @@ ggplot(data=assess_df, aes(sample=residuals)) + geom_qq() + geom_qq_line() + the
 
 ################# Random Slopes ##################
 # Convergence error
+# model2 = lmer(std_log_price ~ object_type + private_offer + rooms + (square_meters | zip_code), data=leverage_removed)
 
 
+################ Checking Missingness ################
+missing_mod = glm((dfrent$rooms == "Missing") ~ object_type + private_offer + square_meters, family=binomial, data=dfrent)
+summary(missing_mod)
 
 
 
